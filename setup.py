664d1173d93e7e178c10b8528baa2cc622ae517a
willcl-ark <will8clark@gmail.com>
2019-01-24 22:10:35 +0000

    updated README.md with github repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="LND gRPC pre-packaged",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/willcl-ark/lnd_grpc",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
