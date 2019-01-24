ac6bbe7dc9e866c093195a1ba1362da720ca01da
willcl-ark <will8clark@gmail.com>
2019-01-24 22:11:54 +0000

    updated README.md with new revision number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lnd_grpc",
-    version="0.0.3",
+    version="0.0.4",
     author="Will Clark",
     author_email="will8clark@gmail.com",
     description="LND gRPC pre-packaged",
