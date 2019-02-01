655edc2f859409c9571f32987f97226816c845d8
willcl-ark <will8clark@gmail.com>
2019-02-01 13:58:23 +0000

    Increased version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lnd_grpc",
-    version="0.2.0",
+    version="0.2.1",
     author="Will Clark",
     author_email="will8clark@gmail.com",
     description="An LND gRPC client for Python 3",
