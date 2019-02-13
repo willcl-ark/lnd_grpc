ee2c590682e898d04691adae210072be4b2c09b9
willcl-ark <will8clark@gmail.com>
2019-02-13 11:48:06 +0000

    Added helper functions to convert from base64 bytes to hex encoded strings
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- lnd_grpc/utilities.py
+++ lnd_grpc/utilities.py
@@ -1,9 +1,13 @@
 import platform
+import codecs
 from pathlib import Path
 from os import environ, path
 
 
 def get_lnd_dir():
+    """
+    Set default LND directory based on detected OS platform
+    """
     lnd_dir = None
     _platform = platform.system()
     home_dir = str(Path.home())
@@ -14,3 +18,21 @@ def get_lnd_dir():
     elif _platform == 'Windows':
         lnd_dir = path.abspath(environ.get('LOCALAPPDATA') + 'Lnd/')
     return lnd_dir
+
+
+def bytes_to_hex_str(base64_bytes):
+    """
+    Convert base64 bytes to hex encoded string
+    """
+    hex_bytes = codecs.encode(base64_bytes, 'hex')
+    hex_str = hex_bytes.decode()
+    return hex_str
+
+
+def hex_str_to_bytes(hex_str):
+    """
+    Convert hex encoded string to base64 bytes
+    """
+    hex_bytes = hex_str.encode()
+    base64_bytes = codecs.decode(hex_bytes, 'hex')
+    return base64_bytes
